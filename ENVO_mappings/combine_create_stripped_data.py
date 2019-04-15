cb51b7b5788cbda9d44b2043cfe8ec380506810e
Chris J Beecroft <cjbeecroft@lbl.gov>
2019-04-15 18:35:38 -0400

    Strip out term and @id out of combined_data.json for hugh's lookup

---
+++ ENVO_mappings/combine_create_stripped_data.py
@@ -0,0 +1,16 @@
+import json
+
+with open('combined_records.json') as json_file:
+    data = json.load(json_file)
+
+output = []
+for record in data:
+    term = record['term']
+    for var in record['envo']:
+        envo = []
+        for t in var['coverageResult']['annotations']:
+            envo.append(t['annotatedClass']['@id'])
+    output.append({'term': term, 'envo': envo})
+
+with open("combined_stripped_data.json", 'w') as f:
+    f.write(json.dumps(output))
